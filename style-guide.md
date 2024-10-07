fa0924640f0ddb43d5bf5a7563b360d62c73cf1d
Tony <tonysalinasdev@gmail.com>
2024-10-07 05:57:43 -0500

    modific home and curse

--- style-guide.md
+++ style-guide.md
@@ -3,7 +3,6 @@
 
 ## Colors
 
-
 primary-color: #3967e1
 secondary-color: #2b2d42
 btn-color-primary: #ffd700
@@ -14,10 +13,9 @@ extra-color: #74c0fc
 
 ### Font
 
-- Family: [Be Vietnam Pro](https://fonts.google.com/specimen/Be+Vietnam+Pro)
-- Weights: 400, 500, 700
-- Family: [Archivo Black](https://fonts.google.com/specimen/Archivo+Black)
-- Weights: 400
+- Family: [Montserrat](https://fonts.google.com/specimen/Montserrat?query=montserrat)
+- Weights: 100 - 900
+
 
 ## Icons
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily:{
-        'vitneam' : ['"Be Vietnam Pro"', 'sans-serif'],
-        'banner' : ['"Archivo Black"', 'sans-serif']
+        'montserrat' : ['"Montserrat"', 'sans-serif'],
       },
       colors:{
         'primary-color' : '#3967e1',
@@ -14,6 +13,9 @@ module.exports = {
         'btn-color-secondary':'#f45d48',   
         'light-color':'#f0f4f8',
         'extra-color':'#74c0fc'
+      },
+      backgroundImage:{
+        'banner-curso': "url('/src/images/banner-curso-home.jpg')",
       }
     },
   },
